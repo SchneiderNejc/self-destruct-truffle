1b625da88bb1ef6c96263a2e0839e6ef8f1acc8f
SchneiderNejc <nejc.sch@gmail.com>
2024-01-22 23:09:51 +0100

    add 1 test

---
+++ test/destruct.js
@@ -0,0 +1,61 @@
+const { assert } = require("console");
+
+let Destruct = artifacts.require("./Destruct.sol");
+
+contract("Self Destruct", async accounts => {
+
+    // contracts
+    let destruct = null;
+
+    // EOAs
+    let owner = accounts[0];
+
+    it("link required contracts", async () => {
+        // initialize contracts
+	    destruct = await Destruct.deployed();
+
+        if (destruct.address != 0)
+            console.log("contract address non zero");
+        else
+            console.log("contract address zero");
+    });
+
+
+
+    it("contract successfully deployed", async () => {
+        await destruct.destroy(owner, { from: owner });
+
+        //contract functions can't be called after it has been destroyed
+        try{
+            await destruct.destroy(owner, { from: owner });
+            assert.fail();
+        } catch (e) {
+            assert(true);
+        }
+
+/*             function isContractAlive(address contractAddress) external view returns(bool) {
+        uint256 codeSize;
+        assembly {
+                    codeSize:= extcodesize(contractAddress)
+                }
+                return codeSize > 0;
+            } */
+    });
+
+    // 0. contract successfuly selfdestructs
+    // 1. owner is message sender
+    // 2. erc 20 is transfered to Destruct
+    // 3. erc 721 is transfered to Destruct
+    // 4. only owner can call Destroy()
+    // 5. both erc 20 and erc 721 are transfered to owner
+    // ---------------------------
+    // Make it so at certain amount of nfts contract is auto destructed
+    // Make it so at certain amount of tokens auto destructed
+
+    // Repeat in Truffle and Hardhat!
+    // More to come, ChainLink, use box next
+
+
+
+
+});
