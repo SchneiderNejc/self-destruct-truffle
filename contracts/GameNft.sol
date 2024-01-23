e9179dddb79152b64d51877af43712ee38b88f09
SchneiderNejc <nejc.sch@gmail.com>
2024-01-23 23:49:26 +0100

    add ERC721 "GameNft" contract

---
+++ contracts/GameNft.sol
@@ -0,0 +1,21 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract GameNft is ERC721 {
+    uint256 private _nextTokenId;
+
+    constructor() ERC721("GameNft", "ITM") {}
+
+    function awardItem(address receiver)
+        public
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _mint(receiver, tokenId);
+
+        return tokenId;
+    }
+}
