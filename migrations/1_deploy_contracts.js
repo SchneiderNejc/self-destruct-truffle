52e31f4d7d467fd160884fa1f51331028f3e75e1
SchneiderNejc <nejc.sch@gmail.com>
2024-01-23 23:34:46 +0100

    update deployment script with ERC20

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,4 +1,8 @@
 var Destruct = artifacts.require("./Destruct.sol");
+var GLDToken = artifacts.require("./GLDToken.sol");
+
+
+var initSupply = 10^6;
 
 
 async function getAccount(id) {
@@ -16,5 +20,9 @@ module.exports = async function (deployer, network) {
 
         await deployer.deploy(Destruct).then(function () {
             console.log("Destruct contract was deployed at address: " + Destruct.address);
+        });
+
+         await deployer.deploy(GLDToken, initSupply).then(function () {
+             console.log("GLDToken contract was deployed at address: " + GLDToken.address);
         });
 };
