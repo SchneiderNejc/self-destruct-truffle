1d871ea13c3e93b88443a358da919944fd22dd6a
SchneiderNejc <nejc.sch@gmail.com>
2024-01-23 23:34:28 +0100

    add ERC20 "GLDToken" contract

---
+++ contracts/GLDToken.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GLDToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Gold", "GLD") {
+        _mint(msg.sender, initialSupply);
+    }
+}
