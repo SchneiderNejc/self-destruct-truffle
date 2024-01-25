de35849605552f3431de67152176c576a5f092ee
SchneiderNejc <nejc.sch@gmail.com>
2024-01-25 20:08:13 +0100

    remove redundant comment section

--- test/destruct.js
+++ test/destruct.js
@@ -87,20 +87,4 @@ contract("Self Destruct", async accounts => {
             "insufficient token balance");
     });
 
-    // 0. contract successfuly selfdestructs
-    // 1. owner is message sender
-    // 2. erc 20 is transfered to Destruct
-    // 3. erc 721 is transfered to Destruct
-    // 4. only owner can call Destroy()
-    // 5. both erc 20 and erc 721 are transfered to owner
-    // ---------------------------
-    // Make it so at certain amount of nfts contract is auto destructed
-    // Make it so at certain amount of tokens auto destructed
-
-    // Repeat in Truffle and Hardhat!
-    // More to come, ChainLink, use box next
-
-
-
-
 });
