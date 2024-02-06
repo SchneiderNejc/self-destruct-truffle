ba35be25200c22fe0f1a2f2e961de7fd7e5b4277
SchneiderNejc <nejc.sch@gmail.com>
2024-02-06 19:06:10 +0100

    update bsctestnet to read mnemonic instead of private key

--- truffle-config.js
+++ truffle-config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
 
     bsctestnet: {
-      provider: () => new HDWalletProvider(process.env.BSC_TESTNET, `https://data-seed-prebsc-1-s1.binance.org:8545/`, 0, 3),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://data-seed-prebsc-1-s1.binance.org:8545/`, 0, 3),
       network_id: 97,
       confirmations: 3,
       timeoutBlocks: 200,
