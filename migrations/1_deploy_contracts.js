deb603eefefa98f1ed7d3b3b88ec90011725389e
SchneiderNejc <nejc.sch@gmail.com>
2024-02-09 18:30:47 +0100

    optimize deployment structure and add comments

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,26 +1,33 @@
+// Import artifacts
 var Destruct = artifacts.require("./Destruct.sol");
 var GLDToken = artifacts.require("./GLDToken.sol");
 var GameNft = artifacts.require("./GameNft.sol");
 
-var initSupply = 10**6; //one million (10^18 = convert wei to ether)
+// Fixed data structure
+var contractNames = [];
+var contractAddresses = [];
 
+// Parameters setup
+var initSupply = 10 ** 6; // One million (10^18 = convert wei to ether)
+var gldToken = "0x9f1DEd9BC243cB2fec733d0E280aECb9f6dA6E2a";
 
+// Accounts data
 module.exports = async function (deployer, network) {
+    let owner = (await web3.eth.getAccounts())[0];
+    console.log("owner: ", owner);
 
-        let owner = (await web3.eth.getAccounts())[0];
-        console.log("owner: " ,owner);
-        var gldToken = "0x9f1DEd9BC243cB2fec733d0E280aECb9f6dA6E2a";
+    // Deploy contracts
+    await deployer.deploy(GLDToken, initSupply).then(function () {
+        contractNames.push("GLDToken");
+        contractAddresses.push(GLDToken.address);
+    });
 
-
-        await deployer.deploy(GLDToken, initSupply).then(function () {
-             console.log("GLDToken deployed at: " + GLDToken.address);
-        });
-
-        await deployer.deploy(GameNft).then(function () {
-            console.log("GameNft deployed at: " + GameNft.address);
-        });
+    await deployer.deploy(GameNft).then(function () {
+        contractNames.push("GameNft");
+        contractAddresses.push(GameNft.address);
+    });
 
     await deployer.deploy(Destruct, GLDToken.address).then(function () {
-            console.log("Destruct deployed at: " + Destruct.address);
-        });
-};
+        contractNames.push("Destruct");
+        contractAddresses.push(Destruct.address);
+    });
